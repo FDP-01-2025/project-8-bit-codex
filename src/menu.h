914e8f22407504285a40cc45267aaca6fbcee86e
Leandro Aquino <00036725@uca.edu.sv>
2025-06-18 21:02:27 -0600

    add file :memo:

---
+++ src/menu.h
@@ -0,0 +1,73 @@
+#ifndef menu_H
+#define menu_H
+
+#include <iostream>
+#include <conio.h>
+#define TECLA_ARRIBA 72
+#define TECLA_ABAJO 80
+#define ENTER 13
+using namespace std;
+
+void menu_principal();
+void menu_new();
+void menu_load();
+
+void menu_principal()
+{
+    int tecla;
+    bool repite = true;
+    int opcion = 1;
+    do
+    {
+        system("cls");
+        cout << "===============================" << endl;
+        cout << "         Tower of souls        " << endl;
+        cout << "===============================" << endl;
+        cout << (opcion == 1 ? "==> " : "   ") << "  New Game" << endl;
+        cout << (opcion == 2 ? "==> " : "   ") << "  Load Game" << endl;
+        cout << (opcion == 3 ? "==> " : "   ") << "  Exit" << endl;
+
+        tecla = getch();
+
+        switch (tecla)
+        {
+        case TECLA_ARRIBA:
+            opcion--;
+            if (opcion < 1)
+                opcion = 3;
+            break;
+        case TECLA_ABAJO:
+            opcion++;
+            if (opcion > 3)
+                opcion = 1;
+            break;
+        case ENTER:
+            switch (opcion)
+            {
+            case 1:
+                menu_new();
+                break;
+            case 2:
+                menu_load();
+                break;
+            case 3:
+                repite = false;
+                break;
+            }
+            break;
+        }
+    }while (repite);
+}
+
+void menu_new()
+{
+    cout <<"New game..."<<endl;
+    system("pause");
+}
+void menu_load()
+{
+    cout<<"Load game..."<<endl;
+    system("pause");
+}
+
+#endif
