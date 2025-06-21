9aa82b3f9ef5ddf233f6367b756e53b8fae85308
Leandro Aquino <00036725@uca.edu.sv>
2025-06-20 21:49:27 -0600

    update :white_check_mark:

--- src/character.h
+++ src/character.h
@@ -13,4 +13,9 @@ struct character
     int object;
 };
 
+const character assasin = {10, 5, 5, 15, 5, 10, 0};
+const character mage    = {8, 8, 3, 5, 20, 6, 0};
+const character knight  = {15, 15, 10, 3, 3, 4, 0};
+const character berserk = {20, 10, 12, 3, 0, 5, 0};
+
 #endif
