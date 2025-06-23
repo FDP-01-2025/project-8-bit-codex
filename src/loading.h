d6fe06ff880c69c64471e623449c4e838b07a248
Leandro Aquino <00036725@uca.edu.sv>
2025-06-22 22:47:14 -0600

    add file :memo:

---
+++ src/loading.h
@@ -0,0 +1,16 @@
+#ifndef loading_H
+#define loading_H
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+void loading(){
+    char x =219;
+    cout <<"========================"<<endl;
+    cout <<"Loading Please Wait     "<<endl;
+    for (int i = 1; i <=20; i++){
+        cout <<x;
+        Sleep(600);
+    }
+}
+#endif
