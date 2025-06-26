ad2e0c05b2fec27d1838c6cfeda50c179fcdec61
Leandro Aquino <00036725@uca.edu.sv>
2025-06-26 17:22:29 -0600

    add file :memo:

---
+++ src/chest.h
@@ -0,0 +1,41 @@
+#ifndef chest_H
+#define chest_H
+#include <iostream>
+#include <cstdlib>
+#include <time.h>
+using namespace std;
+
+void chest()
+{
+    int N;
+    srand(time(NULL));
+    int num = (rand() % 5) + 1;
+
+    if (num == 1)
+    {
+        cout <<"\t\t\t\t===========================\n";
+        cout <<"\t\t\t\tHave found a potion of life\n";
+        cout <<"\t\t\t\t===========================\n";
+    }
+    else if (num == 2)
+    {
+        cout <<"\t\t\t\t===============================\n";
+        cout <<"\t\t\t\tHave found a potion of strength\n";
+        cout <<"\t\t\t\t===============================\n";
+    }
+    else if (num == 3)
+    {
+        cout <<"\t\t\t\t========================\n";
+        cout <<"\t\t\t\tHave found a defense pot\n";
+        cout <<"\t\t\t\t========================\n";
+    }else if(num == 4){
+        cout <<"\t\t\t\t=============\n";
+        cout <<"\t\t\t\tEmpty chest!!\n";
+        cout <<"\t\t\t\t=============\n";
+    }else if(num == 5){
+        cout <<"\t\t\t\t===============\n";
+        cout<<"\t\t\t\tEnemy in sight!!\n";
+        cout <<"\t\t\t\t===============\n";
+    }
+}
+#endif
