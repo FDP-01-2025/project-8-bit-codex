5792bf5252eb56cfeeff5919deab497766a60158
LuisACampos7 <00122425@uca.edu.sv>
2025-07-05 21:31:57 -0600

    map upd

--- src/entities/enemies.h
+++ src/entities/enemies.h
@@ -8,14 +8,14 @@ struct enemies
 };
 
 //normal enemies
-const enemies skeleton = {100000, 5};
-const enemies zombie = {100000, 8};
+const enemies skeleton = {100000, 1};
+const enemies zombie = {100000, 1};
 
 //Boss
-const enemies Dragon = {1900000, 30};
-const enemies Mini_Dragon = {1700000, 15};
-const enemies Dark_knight = {1500000, 10};
-const enemies orc = {1100000, 30};
-const enemies Spider = {1000000, 20};
+const enemies Dragon = {1400000, 7};
+const enemies Mini_Dragon = {1300000, 6};
+const enemies Dark_knight = {1200000, 5};
+const enemies orc = {1100000, 4};
+const enemies Spider = {1000000, 3};
 
 #endif
--- src/prueba_mapa.cpp
+++ src/prueba_mapa.cpp
@@ -3,7 +3,7 @@
 #include <windows.h>
 #include "menu.h"
 #include "Inventory.h"
-#include "mapa.h"
+#include "mapa2.h"
 #include "new.h"
 #include "utils/Screen.h"
 #include "utils/inputs.h"
@@ -13,7 +13,7 @@ int main() {
     character player;
     gotoxy(0,0);
     hideCursor();
-    copia_nivel1();         
+    copia_nivel2();         
     mover_personaje_wasd(player); 
     return 0;
 }
