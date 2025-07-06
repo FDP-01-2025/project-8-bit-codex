8c2aa221dc2850a871f203ca30255235738a5542
LuisACampos7 <00122425@uca.edu.sv>
2025-07-06 17:32:54 -0600

    pasar de mapa :white_check_mark:

--- src/prueba_mapa.cpp
+++ src/prueba_mapa.cpp
@@ -3,7 +3,11 @@
 #include <windows.h>
 #include "menu.h"
 #include "Inventory.h"
+#include "mapa.h"
 #include "mapa2.h"
+#include "mapa3.h"
+#include "mapa4.h"
+#include "mapa5.h"
 #include "new.h"
 #include "utils/Screen.h"
 #include "utils/inputs.h"
@@ -13,7 +17,7 @@ int main() {
     character player;
     gotoxy(0,0);
     hideCursor();
-    copia_nivel2();         
+    copia_nivel1();         
     mover_personaje_wasd(player); 
     return 0;
 }
