7d48c545e24c24a67780c7945e4a8db59f3528af
LuisACampos7 <00122425@uca.edu.sv>
2025-07-07 16:18:27 -0600

    upd

--- src/entities/enemies.h
+++ src/entities/enemies.h
@@ -8,14 +8,14 @@ struct enemies//Stadisticas de un enemigos base
 };
 
 //normal enemies
-const enemies skeleton = {70, 1};
-const enemies zombie = {70, 1};
+const enemies skeleton = {70, 10};
+const enemies zombie = {70, 10};
 
 //Boss
-const enemies Dragon = {800, 10};
-const enemies Mini_Dragon = {600, 8};
-const enemies Dark_knight = {450, 6};
-const enemies orc = {300, 5};
-const enemies Spider = {250, 3};
+const enemies Dragon = {800, 100};
+const enemies Mini_Dragon = {600, 80};
+const enemies Dark_knight = {450, 60};
+const enemies orc = {300, 50};
+const enemies Spider = {250, 30};
 
 #endif
