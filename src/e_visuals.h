0f8d89e7f54993b03e4e8538a8239d15ca1e145c
Alejandro796-ops <00067525@uca.edu.sv>
2025-07-06 23:44:25 -0600

    update :white_check_mark:

---
+++ src/e_visuals.h
@@ -0,0 +1,13 @@
+#ifndef E_VISUALS_H
+#define E_VISUALS_H
+
+extern const char* image_skeleton;
+extern const char* image_zombie;
+
+extern const char* image_dragon;
+extern const char* image_mini_dragon;
+extern const char* image_dark_knight;
+extern const char* image_orc;
+extern const char* image_spider;
+
+#endif
