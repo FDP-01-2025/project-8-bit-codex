5fdb42e0666061b2c8ee78175dc754362f70b536
Alejandro796-ops <00067525@uca.edu.sv>
2025-07-06 21:32:39 -0600

    update :white_check_mark:

--- src/entities/character_visuals.h
+++ src/entities/character_visuals.h
@@ -1,16 +1,16 @@
 #ifndef CHARACTER_VISUALS_H
 #define CHARACTER_VISUALS_H
 
-#include "character.h" // Aquí están definidos los enemigos y sus estadisticas correspondientes
-#include "image.h"     // Aquí están definidas las imágenes ASCII de cada enemigo
+#include "character.h" // Aquí están definidos los personajes y sus estadisticas correspondientes
+#include "image.h"     // Aquí están definidas las imágenes ASCII de cada personaje
 
-// Estructura que combina datos del enemigo + su imagen ASCII
+// Estructura que combina datos del personaje + su imagen ASCII
 struct characterVisuals {
-    character data;         // Datos del enemigo (hp, strength)
+    character data;         // Datos del personaje (hp, strength,etc)
     const char* image;      // Puntero a la imagen ASCII
 };
 
-// Aqui se asocia cada enemigo con su respectiva imagen ASCII para ser usado en un cpp
+// Aqui se asocia cada personaje con su respectiva imagen ASCII para ser usado en un cpp
 extern const characterVisuals image_knight;
 
 extern const characterVisuals image_assasin;
